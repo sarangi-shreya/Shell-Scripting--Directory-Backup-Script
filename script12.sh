7f330ff610abc705b898fb61757090698dc99237
Shreya Sarangi <shreyasarangi2006@gmail.com>
2025-10-26 13:01:13 +0530

    Update script12.sh

--- script12.sh
+++ script12.sh
@@ -1,6 +1,6 @@
 directory_tobackup=$1       # This is the directory we want to zip and create a backup(passed as argument 1)
 
-backup_location= $2         # This the path where we want to store the backup(passed as argument 2)
+backup_location=$2         # This the path where we want to store the backup(passed as argument 2)
 
 current_date=$(date +%Y-%m-%d)	# Value of the current date which will be appended in the backup file
 
