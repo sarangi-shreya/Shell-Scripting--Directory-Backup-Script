5e0df74608bb8791c73bbc082736958f68c525fd
sarangi-shreya <shreyasarangi2006@gmail.com>
2025-10-26 15:47:09 +0530

    Added Count_Dir.sh

---
+++ CountDirectoryFile/Count_Dir.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+directory_to_check=$1 # This is the path of the directory in which files are present(passed as argument 1)
+
+while [ "$(ls -A $directory_to_check)" ]  #till the time directory exists keep running the loop
+do
+    file_count=$(ls -1 $directory_to_check | wc -l)
+    echo "File count in the directory is $file_count"
+    sleep 5
+done
+echo "Directory is now empty!"
